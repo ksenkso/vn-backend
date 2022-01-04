b8f34105c8281675416464138581f688ee302022
ksenkso <yazunya@yandex.ru>
2022-01-04 22:24:39 +0300

    feat(server): move to https

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync(process.env.HTTPS_KEY),
+  cert: fs.readFileSync(process.env.HTTPS_CERT),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.use(cookieParser());
